266f27ba3005ef0292308d65fd0fe6d08d6130ce
Infinity-Ben <165562478+Infinity-Ben@users.noreply.github.com>
2024-06-19 21:04:25 +0000

    added html and css files

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
 
 @app.route("/")
-def Hello_world():
-  return "Hello, Gogo!"
+def Hello_GogoTech():
+  return render_template('index.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
